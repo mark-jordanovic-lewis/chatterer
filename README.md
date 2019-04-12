3f5bd64cb6c61f2d0e9ddfc14a00fa3d348518bc
mark-jordanovic-lewis <mark.4ndrew.lewis@gmail.com>
2019-04-12 23:54:25 +0100

    actually need to write tests now...

--- README.md
+++ README.md
@@ -1,7 +1,59 @@
-# Chatterer
+# Chatterer Systems
 
-This is an app that will chatter. Chatter chatter chatter. I guess I want it to connect to webpage and pull their content, and to be able to interact with APIs, and to provide a channel for people to chat through, possibly as a tunnel.
+A composer system is an internet IO handler. It's tasks are customisable via the thinkers it is holding in its []thinker slice and the conductors that facilitate communications between them (and other disparate packages).
 
-I'll likely build some google API calls into it to get it up and running first.
+- A composer is a http network node
+  - it has a concurrently running system of internal communication and computation nodes
+  - it's functionality is defined by the injected `worker` function types
+    - `worker` takes a channel arg and 'returns' down the same channel
+    - should have multiple duplicate workers for high traffic endpoint
+  - i/o defined by `conductor` function types,
+    -  routes data via channels between `worker` and other conductor functions
 
-Honestly I just wanted to make something with http and channels to see what I could do and it exploded.
+### Bootstrapping
+Going to have to build some examples...
+
+- User defines a set of Conductor and `worker` functions to model their solution
+- functions are injected into Composure to form an async system of processes to handle
+requests.
+- it's basically a microservice
+
+##### HTTP Req
+- serveHTTP sends to server channel.
+- at least one conductor listens on server channel
+- data sent down channel to `worker` which computes result and
+
+- should know about a bunch of clients to respond to
+- should know about all the neighbours that it needs to talk to
+- should be told how to handle inputs (whether from ) via it's thinker functions (written by the user-programmer)
+- should have a set of conductor functions that direct communications between relevant channels (written by the user-programmer)
+- should handle a set of http requests
+  - ie/ be coherent with std REST APIs, externally
+- Be able to consume external APIs via specialised composers
+- The
+
+## Components
+#### Chatterer
+- Chatterers should be flexible enough to instantiate other composers based in input
+- Should handle http requests like a std Go Handler interface
+- Can form the initial stage of some processing pipeline for a response
+
+#### Thinker
+#### Conductor
+- at least one conductor should listen on the server channel
+
+#### Init
+- Initialise the environment
+- Here is where you should pop all the composers that need initialising
+#### There's a logger to deal with too
+
+
+## Flow
+#### Std Handler type
+- HTTP req handled by composer
+- ServeHTTP is as normal, possibly send logging data through conductor
+- Response generated
+
+####
+
+## Todo
--- channel_1.go
+++ channel_1.go
@@ -41,28 +41,51 @@ func init() {
 
 // Microservice Action One \\
 // ----------------------- \\
-type chatterer struct{
+type Worker func(*Composure) (start func(chan []string))// maybe []byte would be better so JSON can be passed
+type Conductor func(*Composure) (start func([]chan []string, chan bool))// type Conductor func([]chan Composure, chan bool)
+type Composure struct{
   name string
   port int
   client *http.Client
   serverChannel chan interface{}   // have to do some clever stuff here to extract what type is sent
-  discussionsPort chan interface{} // have to do some clever stuff here to extract what type is sent
+  discussions map[string]Conductor
+  actions map[string][]Worker       // have to do some clever stuff here to extract what is sent
   quit chan bool
   friends map[string][]struct{
     port int
     suffix string
   }
-  neighbours map[string]<-chan interface{}
+  neighbours map[string]<-chan []string
+}
+// make Composure an implementor of Handler interface
+func (self Composure) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+  decoder := json.NewDecoder(req.Body)
+  var acceptable_params struct{
+    url string
+    port int
+    suffix string
+  }
+  if err := decoder.Decode(&acceptable_params); err != nil {
+    logErrorEvent("INGRESS DECODE ERROR",fmt.Sprintf("Error decoding json: %s", err))
+    io.WriteString(res, fmt.Sprintf("%s", err))
+  } else {
+    // send to server_channel here
+    io.WriteString(res, fmt.Sprintf("extracted_params: %+v", acceptable_params))
+  }
 }
 
 // open a listening channel`
-func (self chatterer) listen() {
+func (self Composure) listen() {
   http.ListenAndServe(fmt.Sprintf(":%d", self.port), self.mux())
 }
 
-// run loop for chatterer operations
-func (self *chatterer) runChatterer() {
+// run loop for Composure operations
+func (self  Composure) run(channels map[string][]chan []string) {
   go self.listen()
+  for title, conversation := range self.discussions {
+    if channel, ok := channels[title]; ok { conversation(&self)(channel, self.quit) }
+  }
+  // need to know what to do about boot strapping the workers
   for {
     select {
     case <- self.quit:
@@ -74,30 +97,14 @@ func (self *chatterer) runChatterer() {
 }
 
 // router
-func (self chatterer) mux() *http.ServeMux {
+func (self Composure) mux() *http.ServeMux {
   mux := http.NewServeMux()
   mux.Handle(fmt.Sprintf("/%s/", self.name), self)
   return mux
 }
 
-// make chatterer an implementor of Handler interface
-func (self chatterer) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-  decoder := json.NewDecoder(req.Body)
-  var acceptable_params struct{
-    url string
-    port int
-    suffix string
-  }
-  if err := decoder.Decode(&acceptable_params); err != nil {
-    logErrorEvent("INGRESS DECODE ERROR",fmt.Sprintf("Error decoding json: %s", err))
-  } else {
-    io.WriteString(res, fmt.Sprintf("extraction of params performed: ", acceptable_params))
-
-  }
-}
-
 // send because _everything_ is a POST req in my world - <JSON>is the way</JSON>
-func (self chatterer) send(url, suffix string, port int) (*http.Response, error) {
+func (self Composure) send(url, suffix string, port int) (*http.Response, error) {
   json_body, err := buildBody(url, suffix, port)
   if err != nil { logErrorEvent("FAILED SEND", fmt.Sprintf("Error marshaling json_body in buildBody(): %s", err)) }
   addr := fmt.Sprintf("%s:%d/%s", url, port, suffix)
@@ -133,18 +140,28 @@ func post(url string, body []byte, client *http.Client) (*http.Response, error) 
 // Conductor functions \\
 // =================== \\
 // can use this for a map or slice of discussions:
-//   discussions := []conductor{ monologue, dialogue, ..., forum }
-type conductor func(chan chatterer, chan chatterer, chan bool)
-
-func dialogue(chan_one chan chatterer, chan_two chan chatterer, kill chan bool) {
+//   discussions := []Conductor{ monologue, dialogue, ..., forum }
+// think of it like an interface for function types
+//
+// func discussion()
+// - map[string]chan Composure
+// - channels are bi-directional
+func dialogue(channels []chan []string, kill chan bool) error { // func dialogue(channels []chan Composure, kill chan bool) error {
+  if len(channels) != 2 { return errors.New("Conductor Error: dialogue - channels must have two elements.") }
   for {
     select {
-    case one := <- chan_one:
-      fmt.Println(one.name, "is listening on", one.port , "found in one")
-    case two := <- chan_two:
-      fmt.Println(two.name, "is listening on", two.port, "found in two")
+    case one := <- channels[0]:
+      // parse string in relevant way
+      // send to channel two
+      fmt.Println("got:", one, "on channel 1")
+      // channels[1] <- channels[0]
+    case two := <- channels[1]:
+      // parse string in relevant way
+      // send to channel one
+      fmt.Println("got:", two, "on channel 2")
+      // channels[0] <- channels[1]
     case <- kill:
-      return
+      return nil
     default:
       time.Sleep(time.Duration(50*time.Millisecond))
       fmt.Print(".")
@@ -163,15 +180,23 @@ func main() {
   }()
   rand.Seed(time.Now().UnixNano())
 
-  chan_one := make(chan int)
-  chan_two := make(chan int)
-
+  chan_one := make(chan []string)
+  chan_two := make(chan []string)
+  chan_one <- []string{"beep"}
+  go func() { <-chan_one }()
+  chan_two <- []string{"beep"}
+  go func() { <-chan_two }()
+  chan_one = make(chan []string)
 }
 
 // ================= \\
 // Logging Functions \\
 // ================= \\
 // initialise the logger
+// - make a:
+//     channel log
+//     error log
+//     ingress log
 func initialiseLogger() {
   // make and set location of log file
   date := time.Now().Format("2006-01-02")
@@ -189,7 +214,7 @@ func initialiseLogger() {
 // log to file
 func logErrorEvent(logLevel string, message string) error {
   _, file, no, ok := runtime.Caller(1)
-  logLineData := "logger_server.go"
+  var logLineData string
   if ok {
     file = shortenFilePath(file)
     logLineData = fmt.Sprintf("%s ==== [ ERROR : %s ] %s : %d - %s", timestamp(), logLevel, file,  no, message)
@@ -202,10 +227,23 @@ func logErrorEvent(logLevel string, message string) error {
 
 func logIngressEvent(logLevel string, message string) {
   _, file, no, ok := runtime.Caller(1)
-  logLineData := "logger_server.go"
+  var logLineData string
   if ok {
     file = shortenFilePath(file)
     logLineData = fmt.Sprintf("%s - %s %s : %d - %s", timestamp(), logLevel, file, no, message)
+  } else {
+    logLineData = fmt.Sprintf("%s - %s %s (Caller not found) : %d - %s", timestamp(), logLevel, file, no, message)
+  }
+  logger.Println(logLineData)
+}
+
+func logChannelEvent(logLevel string, message string) {
+  // direction to find channel easier --> | <-- | <-->
+  _, file, no, ok := runtime.Caller(1)
+  var logLineData string
+  if ok {
+    file = shortenFilePath(file)
+    logLineData = fmt.Sprintf("%s - %s %s : %d <---------> %s", timestamp(), logLevel, file, no, message)
   } else {
     logLineData = fmt.Sprintf("%s - %s %s (Caller not found) : %d - %s", timestamp(), logLevel, file, no, message)
   }
